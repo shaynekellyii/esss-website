a1ca52cd2fb656ab40e650016ef1b6c736dcbffe
Shayne Kelly II <skshayne@gmail.com>
2016-02-17 16:12:56 -0800

    Changed src to ng-src, #27

--- js/controllers.js
+++ js/controllers.js
@@ -2,11 +2,11 @@ var esssApp = angular.module('esssApp', []);
 
 esssApp.controller('SliderCtrl', function ($scope) {
   $scope.slides = [
-    {'img': '/esss-website/banner1.jpg',
+    {'img': '/esss-website/img/banner1.jpg',
      'align': 'center-align',
      'title': 'Welcome',
      'description': 'We are the Engineering Science Student Society of SFU.'},
-    {'img': '/esss-website/banner2.jpg',
+    {'img': '/esss-website/img/banner2.jpg',
      'align': 'left-align',
      'title': 'Get involved',
      'description': 'Our Spring semester meetings are held Mondays at 1:30 in ASB 9705.'}
